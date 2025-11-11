c66e3d7c7fbb1ee06258d0355efce1b7690dd9b1
bryanvonmeeteren <b.vonmeeteren@student.han.nl>
2025-11-11 13:42:38 +0100

    refactored some code

--- UnityProject/WhileYoureHere/Assets/Scripts/MovementController.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/MovementController.cs
@@ -5,18 +5,21 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Animator))]
 public class MovementController : MonoBehaviour
 {
+    [Header("Movement Variables")]
     private float _moveX;
     private float _moveY;
 
-    public float movementSpeed = 2.2f;
+    [Header("Movement Speed")]
+    [SerializeField] private float movementSpeed = 2.2f;
 
-    Animator animator;
-    CharacterController controller;
+    [Header("Components for animation & character controller")]
+    Animator _animator;
+    CharacterController _controller;
 
     void Awake()
     {
-        animator = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
     }
 
     void OnMove(InputValue value)
@@ -28,51 +31,12 @@ public class MovementController : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("the value of _moveX is: " + _moveX);
-        Debug.Log("the value of _moveY is: " + _moveY);
-
-        bool pressingW = _moveY > 0f;
-        bool pressingS = _moveY < 0f;
-
-        bool pressingA = _moveX < 0f;
-        bool pressingD = _moveX > 0f;
-
-        bool isIdle = _moveY == 0f;
-
-        if (pressingW)
-        {
-            animator.SetBool("isWalking", true);
-        }
-        if (!pressingW)
-        {
-            animator.SetBool("isWalking", false);
-        }
-        if (pressingS)
-        {
-            animator.SetBool("isWalkingBackwards", true);
-        }
-        if (!pressingS)
-        {
-            animator.SetBool("isWalkingBackwards", false);
-        }
-        if (pressingA)
-        {
-            animator.SetBool("isStrafingLeft", true);
-        }
-        if (!pressingA)
-        {
-            animator.SetBool("isStrafingLeft", false);
-        }
-        if (pressingD)
-        {
-            animator.SetBool("isStrafingRight", true);
-        }
-        if (!pressingD)
-        {
-            animator.SetBool("isStrafingRight", false);
-        }
+        _animator.SetBool("isWalking", _moveY > 0);
+        _animator.SetBool("isWalkingBackwards", _moveY < 0);
+        _animator.SetBool("isStrafingLeft", _moveX < 0);
+        _animator.SetBool("isStrafingRight", _moveX > 0);
         
         Vector3 movementFinal = new(_moveX, 0.0f, _moveY);
-        controller.Move(movementSpeed * Time.deltaTime * movementFinal);
+        _controller.Move(movementSpeed * Time.deltaTime * movementFinal);
     }
 }
