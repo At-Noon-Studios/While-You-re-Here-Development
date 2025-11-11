1b48daa292d967ab56ab8b138766aec11f385855
LucasvanSteveninck <L.vansteveninck@student.han.nl>
2025-11-11 14:18:17 +0100

    compiler error

--- UnityProject/WhileYoureHere/Assets/Scripts/Vector2EventChannel.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/Vector2EventChannel.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Vector2EventChannel", menuName = "ScriptableObjects/Events/Vector2EventChannel")]
 public class Vector2EventChannel : ScriptableObject
-{ 
+{
         public Action<Vector2> OnRaise;
 
         public void Raise(Vector2 value)
-        { 
+        {
                 OnRaise?.Invoke(value);
         }
 }
