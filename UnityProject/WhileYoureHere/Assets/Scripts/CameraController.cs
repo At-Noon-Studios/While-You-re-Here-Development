b7f10fc3e2c752bd11303782dad8bcba67277595
LucasvanSteveninck <L.vansteveninck@student.han.nl>
2025-11-11 14:48:31 +0100

    cursor is now hidden

--- UnityProject/WhileYoureHere/Assets/Scripts/CameraController.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/CameraController.cs
@@ -15,6 +15,11 @@ public class CameraController : MonoBehaviour
     private float _xRotation;
     private float _yRotation;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     private void OnEnable()
     {
         look.OnRaise += OnLookInput;
@@ -30,6 +35,7 @@ public class CameraController : MonoBehaviour
         _yRotation += (mouseDelta.x * data.Sensitivity) % CircleDegrees;
         _xRotation += (-mouseDelta.y * data.Sensitivity) % CircleDegrees;
         _xRotation = Mathf.Clamp(_xRotation, data.MinYAngle, data.MaxYAngle);
+        
         var rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
         transform.rotation = rotation;
         OnRotate?.Invoke(rotation);
