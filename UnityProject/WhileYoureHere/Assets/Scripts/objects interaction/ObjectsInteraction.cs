28c7bf43ebcca22edb419d45150841900f3b2887
Akramhd <ma.haddad@student.han.nl>
2025-11-12 16:58:41 +0100

    first small step were taking ✈️
    Hello chat

---
+++ UnityProject/WhileYoureHere/Assets/Scripts/objects interaction/ObjectsInteraction.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ObjectsInteraction : MonoBehaviour
+{
+    /* TODO:: add a raycast that detects when objects are hitting when E is pressed
+        add a layer mask var in both unity and code
+        add script to player if already exists
+        
+    */
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
