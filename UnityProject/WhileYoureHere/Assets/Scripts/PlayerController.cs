a4dbc65817eba2374c3db2691030d7001a2a55ac
LucasvanSteveninck <L.vansteveninck@student.han.nl>
2025-11-13 11:07:36 +0100

    commit

--- UnityProject/WhileYoureHere/Assets/Scripts/PlayerController.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,45 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CameraController playerCamera;
+    [Header("Listen to")]
+    [SerializeField] private Vector2EventChannel move;
+    
+    private CharacterController _characterController;
+    private Vector3 _currentMovementVector;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _characterController.Move(_currentMovementVector * Time.deltaTime);
+    }
 
     private void OnEnable()
     {
         playerCamera.OnRotate += RotatePlayerBody;
+        move.OnRaise += OnMoveInput;
     }
 
     private void OnDisable()
     {
         playerCamera.OnRotate -= RotatePlayerBody;
+        move.OnRaise -= OnMoveInput;
     }
 
     private void RotatePlayerBody(Quaternion rotation)
     {
         var currentRotation = transform.rotation.eulerAngles;
         transform.rotation = Quaternion.Euler(currentRotation.x, rotation.eulerAngles.y, currentRotation.z);
+    }
+
+    private void OnMoveInput(Vector2 input)
+    {
+        _currentMovementVector = new Vector3(input.x, 0, input.y);
     }
 }
--- UnityProject/WhileYoureHere/Assets/Scripts/PlayerInputProcessor.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/PlayerInputProcessor.cs
@@ -5,9 +5,15 @@ public class PlayerInputProcessor : MonoBehaviour
 {
     [Header("Publish to")] 
     [SerializeField] private Vector2EventChannel look;
+    [SerializeField] private Vector2EventChannel move;
         
     private void OnLook(InputValue inputValue)
     {
         look.Raise(inputValue.Get<Vector2>());
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        move.Raise(inputValue.Get<Vector2>());
     }
 }
