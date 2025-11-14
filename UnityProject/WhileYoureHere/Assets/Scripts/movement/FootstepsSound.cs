14d39257bdab7c34919ae9d47e007afbf6970893
bryanvonmeeteren <b.vonmeeteren@student.han.nl>
2025-11-14 11:19:05 +0100

    different footsteps play on different surfaces

---
+++ UnityProject/WhileYoureHere/Assets/Scripts/movement/FootstepsSound.cs
@@ -0,0 +1,74 @@
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using Vector3 = UnityEngine.Vector3;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(MovementController))]
+public class FootstepsSound : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip[] leaves;
+    [SerializeField] AudioClip[] snow;
+    [SerializeField] AudioClip[] grass;
+    [SerializeField] AudioClip[] floor;
+
+    // Camera mainCamera;
+
+    [SerializeField] Transform raycastHelper;
+
+
+
+    [Header("Variables for footstep frequency")]
+    public float time;
+    public float footstepOffset = 0.5f;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.playOnAwake = false;
+        // mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        HandleFootsteps();
+    }
+
+    void HandleFootsteps()
+    {
+        int layerMask = 1 << 6;
+        layerMask = ~layerMask;
+
+        time -= Time.deltaTime;
+        if (time < 0.0f)
+        {
+            if (Physics.Raycast(transform.position + Vector3.up * .01f, Vector3.down, out RaycastHit hit, 1f, layerMask))
+            {
+                Debug.Log("I am hitting: " + hit.collider.name);
+                Debug.DrawRay(raycastHelper.transform.position, Vector3.down * .01f, Color.red, 60);
+                switch (hit.collider.tag)
+                {
+                    case "GROUND/snow":
+                        _audioSource.PlayOneShot(snow[Random.Range(0, snow.Length - 1)]);
+                        break;
+                    case "GROUND/leaves":
+                        _audioSource.PlayOneShot(leaves[Random.Range(0, leaves.Length - 1)]);
+                        break;
+                    case "GROUND/floor":
+                        _audioSource.PlayOneShot(floor[Random.Range(0, floor.Length - 1)]);
+                        break;
+                    case "GROUND/grass":
+                        _audioSource.PlayOneShot(grass[Random.Range(0, grass.Length - 1)]);
+                        break;
+                    default:
+                        _audioSource.PlayOneShot(floor[Random.Range(0, floor.Length - 1)]);
+                        break;
+                }
+            }
+            time = footstepOffset;
+        }
+    }
+}
