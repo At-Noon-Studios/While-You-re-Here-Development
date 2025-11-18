5ca2c5526054ba6031f6650a8b768f5daa8d7334
Akramhd <ma.haddad@student.han.nl>
2025-11-18 15:22:28 +0100

    Radio second version
    Radio is function as expected, but the UI should be dynamically scaled and the  buttons should always stay on the radio
    
    __ Tomorrow 19-11 i want to make sure the merged version from dev works with my radio
    - UI should stay in place and should only be visible when interacted with the radio.

--- UnityProject/WhileYoureHere/Assets/Scripts/radio interaction/RadioController.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/radio interaction/RadioController.cs
@@ -7,37 +7,49 @@ public class RadioController : MonoBehaviour
     [SerializeField] private AudioClip staticClip;
     [SerializeField] private Slider tuneSlider;
     [SerializeField] private Button startButton;
-
     private AudioSource audioSource;
     private int currentRadioIndex = -1;
     private float tuneThreshold = 0.1f;
-    private bool radioOn = false;
-
+    private bool radioOn;
+    private bool previousRadioState;
+    
+    
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         startButton.onClick.AddListener(TurnRadioOn);
+
     }
 
     private void Update()
     {
+
         if (radioOn)
         {
             TuneRadio();
         }
+
+        // Detect when radio was turned off
+        if (!radioOn && previousRadioState)
+        {
+            Debug.Log("Radio turned OFF — stopping audio.");
+            audioSource.Stop();
+            currentRadioIndex = -1;
+        }
+
+        previousRadioState = radioOn;
     }
 
     private void TurnRadioOn()
     {
-        Debug.Log("Radio On");
-        radioOn = true;
+        radioOn=!radioOn;
     }
 
     private void TuneRadio()
     {
         var value = tuneSlider.value;
         var stationSpacing = 1f / radioTracks.Length;
-        
+
         var newIndex = Mathf.FloorToInt(value / stationSpacing);
         var stationCenter = newIndex * stationSpacing;
 
@@ -60,7 +72,7 @@ public class RadioController : MonoBehaviour
 
     private void PlayClip(AudioClip clip)
     {
-        if (clip is null) return;
+        if (audioSource.clip == clip && audioSource.isPlaying) return;
         audioSource.Stop();
         audioSource.clip = clip;
         audioSource.loop = true;
--- UnityProject/WhileYoureHere/Assets/Scripts/radio interaction/RadioInteraction.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/radio interaction/RadioInteraction.cs
@@ -1,9 +1,10 @@
 using System;
+using Interactable;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class RadioInteraction : MonoBehaviour//, IInteractible
+public class RadioInteraction : InteractableBehaviour//, IInteractible
 {
     /* TODO:: add a raycast that detects when objects are hitting when E is pressed
         add a layer mask var in both unity and code
@@ -13,8 +14,9 @@ public class RadioInteraction : MonoBehaviour//, IInteractible
     [SerializeField] private LayerMask radioLayerMask;
     [SerializeField] private Camera cam;
     [SerializeField] private float lookDistance = 100f;
-    [SerializeField] private GameObject onSwitch;
-    [SerializeField] private GameObject offSwitch;
+    // [SerializeField] private GameObject onSwitch;
+    // [SerializeField] private GameObject offSwitch;
+    [SerializeField] private GameObject canvas;
     private bool _isOn;
     private bool interacted;
     // private Interactible interactible;
@@ -24,13 +26,17 @@ public class RadioInteraction : MonoBehaviour//, IInteractible
         _isOn = false;
     }
 
-
+    void Update()
+    {
+        // Interact();
+        
+    }
     private void OnInteract(InputValue inputValue)
     {
-        Interact();
+        // Interact();
     }
 
-    public void Interact()
+    public override void Interact()
     {
         //wat gebeudrd er wanneer je e drukt op een radio
         
@@ -40,50 +46,52 @@ public class RadioInteraction : MonoBehaviour//, IInteractible
         if (Physics.Raycast(ray, out hit, lookDistance,radioLayerMask))
         {
 
+            Debug.Log(hit.collider.gameObject.name + " was hit");
+            canvas.SetActive(true);
             // if (hit.collider.GetComponent<IInteractible>())
             // {
-            if (hit.collider.gameObject == onSwitch)
-            {
-                    _isOn = true;
-                    
-                    // optional add a light gameObject and toggle it on and off 
-                    
-            }
-            if (hit.collider.gameObject == offSwitch)
-            {
-                _isOn = false;
-                Debug.Log("Object" + hit.collider.gameObject.name + "is hitten");
-            }
+            // if (hit.collider.gameObject == onSwitch)
+            // {
+            //         _isOn = true;
+            //         
+            //         // optional add a light gameObject and toggle it on and off 
+            //         
+            // }
+            // if (hit.collider.gameObject == offSwitch)
+            // {
+            //     _isOn = false;
+            //     Debug.Log("Object" + hit.collider.gameObject.name + "is hitten");
+            // }
 
-            handleOnOffSwitch();
+            // handleOnOffSwitch();
             // }
         }
+        else canvas.SetActive(false);
+        
+        
         Debug.DrawRay(ray.origin, ray.direction*100f, Color.red);
     }
+    
 
-    private void handleOnOffSwitch()
-    {
-        Renderer OnRenderer = onSwitch.GetComponent<Renderer>();
-        Renderer OffRenderer = offSwitch.GetComponent<Renderer>();
+    // private void handleOnOffSwitch()
+    // {
+    //     Renderer OnRenderer = onSwitch.GetComponent<Renderer>();
+    //     Renderer OffRenderer = offSwitch.GetComponent<Renderer>();
+    //
+    //     if (_isOn)
+    //     {
+    //         OffRenderer.material.color = Color.silver;
+    //         OnRenderer.material.SetColor("_Color", Color.green);
+    //         OnRenderer.material.color = Color.green;
+    //         Debug.Log("on switch is on");
+    //     }
+    //     else if (!_isOn)
+    //     {
+    //         OnRenderer.material.color = Color.silver;
+    //         OffRenderer.material.SetColor("_Color", Color.red);
+    //         OffRenderer.material.color = Color.red;
+    //         Debug.Log("off switch is off");
+    //     }
+    // }
 
-        if (_isOn)
-        {
-            OffRenderer.material.color = Color.silver;
-            OnRenderer.material.SetColor("_Color", Color.green);
-            OnRenderer.material.color = Color.green;
-            Debug.Log("on switch is on");
-        }
-        else if (!_isOn)
-        {
-            OnRenderer.material.color = Color.silver;
-            OffRenderer.material.SetColor("_Color", Color.red);
-            OffRenderer.material.color = Color.red;
-            Debug.Log("off switch is off");
-        }
-    }
-    void Update()
-    {
-            
-        
-    }
 }
