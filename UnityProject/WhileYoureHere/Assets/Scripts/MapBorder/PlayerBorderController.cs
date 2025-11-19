6ea2877df48193a9b525fbe9bb4b643ec85e339b
unknown <wj.meeder@student.han.nl>
2025-11-19 10:03:25 +0100

    fixed namespace

--- UnityProject/WhileYoureHere/Assets/Scripts/MapBorder/PlayerBorderController.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/MapBorder/PlayerBorderController.cs
@@ -5,63 +5,72 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class PlayerBorderController : MonoBehaviour
+namespace MapBorder
 {
-    [Header("Border effect settings")] 
-    [SerializeField] private int distanceToTriggerEffects = 5;
-    [SerializeField] private int timeBeforeRetrigger = 10;
-    
-    [Header("Audio clips")]
-    [SerializeField] private AudioClip tooFarFromCabinVoiceLine;
-    
-    private GameObject _player;
-    private Vignette _vignette;
-    private ColorAdjustments _colorAdjustments;
-    private Collider _collider;
-    private AudioSource _audioSource;
-    private bool _blockEffectRetrigger;
-    private GameObject[] _borderObjects;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerBorderController : MonoBehaviour
     {
-        _player =  GameObject.Find("Player");
-        _audioSource = _player.GetComponent<AudioSource>();
-        Volume volume = GetComponent<Volume>();
-        volume.profile.TryGet(out _vignette);
-        volume.profile.TryGet(out _colorAdjustments);
-        _borderObjects = GameObject.FindGameObjectsWithTag("MapBorder");
-    }
+        [Header("Border effect settings")] [SerializeField]
+        private int distanceToTriggerEffects = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        var distances = new List<float>();
-        foreach (GameObject borderObject in _borderObjects)
+        [SerializeField] private int timeBeforeRetrigger = 10;
+
+        [Header("Audio clips")] [SerializeField]
+        private AudioClip tooFarFromCabinVoiceLine;
+
+        private GameObject _player;
+        private Vignette _vignette;
+        private ColorAdjustments _colorAdjustments;
+        private Collider _collider;
+        private AudioSource _audioSource;
+        private bool _blockEffectRetrigger;
+        private GameObject[] _borderObjects;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            borderObject.TryGetComponent<Collider>(out  _collider);
-            distances.Add((_player.transform.position - Physics.ClosestPoint(_player.transform.position, _collider, _collider.transform.position, _collider.transform.rotation)).sqrMagnitude);
+            _player = GameObject.Find("Player");
+            _audioSource = _player.GetComponent<AudioSource>();
+            Volume volume = GetComponent<Volume>();
+            volume.profile.TryGet(out _vignette);
+            volume.profile.TryGet(out _colorAdjustments);
+            _borderObjects = GameObject.FindGameObjectsWithTag("MapBorder");
         }
 
-        var closestBorder = distances.Min();
-        if (closestBorder < distanceToTriggerEffects * distanceToTriggerEffects)
+        // Update is called once per frame
+        void Update()
         {
-            if (!_blockEffectRetrigger) StartCoroutine(PlayVoiceLine());
-            _vignette.intensity.value = Mathf.Clamp(1 - closestBorder / (distanceToTriggerEffects * distanceToTriggerEffects), 0f, 0.5f);
-            _colorAdjustments.saturation.value = Mathf.Clamp((distanceToTriggerEffects * distanceToTriggerEffects - closestBorder) / (distanceToTriggerEffects * distanceToTriggerEffects) * -100, -100, 0);
+            var distances = new List<float>();
+            foreach (GameObject borderObject in _borderObjects)
+            {
+                borderObject.TryGetComponent<Collider>(out _collider);
+                distances.Add((_player.transform.position - Physics.ClosestPoint(_player.transform.position, _collider,
+                    _collider.transform.position, _collider.transform.rotation)).sqrMagnitude);
+            }
+
+            var closestBorder = distances.Min();
+            if (closestBorder < distanceToTriggerEffects * distanceToTriggerEffects)
+            {
+                if (!_blockEffectRetrigger) StartCoroutine(PlayVoiceLine());
+                _vignette.intensity.value =
+                    Mathf.Clamp(1 - closestBorder / (distanceToTriggerEffects * distanceToTriggerEffects), 0f, 0.5f);
+                _colorAdjustments.saturation.value =
+                    Mathf.Clamp(
+                        (distanceToTriggerEffects * distanceToTriggerEffects - closestBorder) /
+                        (distanceToTriggerEffects * distanceToTriggerEffects) * -100, -100, 0);
+            }
+            else
+            {
+                _vignette.intensity.value = 0f;
+                _colorAdjustments.saturation.value = 0;
+            }
         }
-        else
+
+        private IEnumerator PlayVoiceLine()
         {
-            _vignette.intensity.value = 0f;
-            _colorAdjustments.saturation.value = 0;
+            _blockEffectRetrigger = true;
+            _audioSource.PlayOneShot(tooFarFromCabinVoiceLine);
+            yield return new WaitForSeconds(timeBeforeRetrigger);
+            _blockEffectRetrigger = false;
         }
-    }
-    
-    private IEnumerator PlayVoiceLine()
-    {
-        _blockEffectRetrigger = true;
-        _audioSource.PlayOneShot(tooFarFromCabinVoiceLine);
-        yield return new WaitForSeconds(timeBeforeRetrigger);
-        _blockEffectRetrigger = false;
     }
 }
