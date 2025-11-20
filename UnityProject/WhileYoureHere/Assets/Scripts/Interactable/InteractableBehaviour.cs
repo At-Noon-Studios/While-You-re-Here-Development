012ff17e6c0ba09229bb3b63d5618b273566ca1c
LucasvanSteveninck <L.vansteveninck@student.han.nl>
2025-11-20 09:51:51 +0100

    Made it so that InteractableBehaviour will outline all renderers of children instead of only its own renderer component

--- UnityProject/WhileYoureHere/Assets/Scripts/Interactable/InteractableBehaviour.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/Interactable/InteractableBehaviour.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 namespace Interactable
 {
-    [RequireComponent(typeof(Renderer))]
     [RequireComponent(typeof(Collider))] //Not used in this script, but if you add InteractableBehaviour to something that doesn't have a collider you will never receive an 'Interact' callback.
     public abstract class InteractableBehaviour : MonoBehaviour, IInteractable
     {
         private Material _outlineMaterial;
         private UIManager _uiManager;
-        private Renderer _renderer;
+        private Renderer[] _renderers;
 
         private const string OutlineMaterialResourcePath = "OutlineMaterial";
 
         protected void Awake()
         {
-            _renderer = GetComponent<Renderer>();
+            _renderers = GetComponentsInChildren<Renderer>();
+            if (_renderers == null || _renderers.Length == 0) Debug.LogWarning("Scene contains an InteractableBehaviour without any renderers.");
             _outlineMaterial = Resources.Load<Material>(OutlineMaterialResourcePath);
             _uiManager = UIManager.Instance;
         }
@@ -25,31 +25,49 @@ namespace Interactable
         
         public virtual void OnHoverEnter()
         {
-            AddOutlineMaterialToRenderer();
+            AddOutlineMaterialToRenderers();
             _uiManager?.ShowInteractPrompt(gameObject.name);
         }
 
         public virtual void OnHoverExit()
         {
-            RemoveOutlineMaterialFromRenderer();
+            RemoveOutlineMaterialFromRenderers();
             _uiManager?.HideInteractPrompt();
         }
         
-        private void AddOutlineMaterialToRenderer()
+        private void AddOutlineMaterialToRenderers()
         {
-            if (!_renderer || !_outlineMaterial) return;
-            var materials = _renderer.materials;
-            if (materials.Any(m => m.name.StartsWith(_outlineMaterial.name)))
-                return;
-            _renderer.materials = materials.Append(_outlineMaterial).ToArray();
+            foreach (var rendererComponent in _renderers)
+            {
+                AddOutlineMaterialToRenderer(rendererComponent);
+            }
         }
 
-        private void RemoveOutlineMaterialFromRenderer() {
-            if (!_renderer || !_outlineMaterial) return;
-            var materials = _renderer.materials;
+        private void AddOutlineMaterialToRenderer(Renderer rendererComponent)
+        {
+            if (!rendererComponent || !_outlineMaterial) return;
+            var materials = rendererComponent.materials;
+            if (materials.Any(m => m.name.StartsWith(_outlineMaterial.name)))
+                return;
+            rendererComponent.materials = materials.Append(_outlineMaterial).ToArray();
+        }
+        
+
+        private void RemoveOutlineMaterialFromRenderers()
+        {
+            foreach (var rendererComponent in _renderers)
+            {
+                RemoveOutlineMaterialFromRenderer(rendererComponent);
+            }
+        }
+
+        private void RemoveOutlineMaterialFromRenderer(Renderer rendererComponent)
+        {
+            if (!rendererComponent || !_outlineMaterial) return;
+            var materials = rendererComponent.materials;
             if (!materials.Any(m => m.name.StartsWith(_outlineMaterial.name)))
                 return;
-            _renderer.materials = materials.Where(m => !m.name.StartsWith(_outlineMaterial.name)).ToArray();
+            rendererComponent.materials = materials.Where(m => !m.name.StartsWith(_outlineMaterial.name)).ToArray();
         }
     }
 }
