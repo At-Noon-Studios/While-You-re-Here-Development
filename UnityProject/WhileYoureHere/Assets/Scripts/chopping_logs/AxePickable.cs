9a765f92043810fac207eefb5dae8f8f5fc0c9c5
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2025-11-24 14:40:22 +0100

    merge from dev

--- UnityProject/WhileYoureHere/Assets/Scripts/chopping_logs/AxePickable.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/chopping_logs/AxePickable.cs
@@ -1,9 +1,22 @@
+using Interactable;
 using picking_up_objects;
+using UnityEngine;
 
 namespace axe
 {
     public class AxePickable : Pickable
     {
+
+        private void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void Interact()
+        {
+            Debug.Log("Interacting with Axe Pickable");
+            base.Interact();
+        }
         
     }
 }
