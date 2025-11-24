40423f12d032632d381f77bdc40822645c0b3091
Michelle <mba3810@students.fhv.at>
2025-11-24 16:45:29 +0100

    watering can is interactable

--- UnityProject/WhileYoureHere/Assets/Scripts/Interactable/WateringCanInteraction.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/Interactable/WateringCanInteraction.cs
@@ -1,23 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
 namespace Interactable
 {
-    /*public class WateringCanInteraction : InteractableBehaviour
+    public class WateringCanInteraction : InteractableBehaviour
     {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
         }
 
         public override void Interact()
         {
-            // Implement interaction
-            print("Interact with watering can");
+            transform.localEulerAngles = new Vector3(0, 0, 45);
+            print("rotate watering can to water");
         }
-    }*/
+    }
 }
