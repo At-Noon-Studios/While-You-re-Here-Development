5f446499d9897bce2a83fca98f146b4087e1f93d
LucasvanSteveninck <L.vansteveninck@student.han.nl>
2025-11-27 12:00:49 +0100

    changes to pickable system

---
+++ UnityProject/WhileYoureHere/Assets/Scripts/Interactable/Holdable/IHoldableObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Interactable.Holdable
+{
+    public interface IHoldableObject : IInteractable
+    {
+        public float Weight { get; }
+
+        public void Drop();
+        
+        public void Place(Vector3 position, Quaternion? rotation = null);
+    }
+}
