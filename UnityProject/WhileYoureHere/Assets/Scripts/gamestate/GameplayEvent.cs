4d6e559793042a193997913be48ef6612a818f8c
unknown <wj.meeder@student.han.nl>
2025-12-01 10:16:29 +0100

    bug fixes

--- UnityProject/WhileYoureHere/Assets/Scripts/gamestate/GameplayEvent.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/gamestate/GameplayEvent.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using chore;
-using UnityEditor;
+using ScriptableObjects.chores;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Video;
