2d2cdc89dc6482754c14ff96ee886b498e7014a8
Michelle <mba3810@students.fhv.at>
2025-12-08 14:34:45 +0100

    squash! Revert "Merge branch 'development' into 47-gardening"

--- UnityProject/WhileYoureHere/Assets/Scripts/entity/WateringCan.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/entity/WateringCan.cs
@@ -1,4 +1,5 @@
 using chore;
+using Interactable;
 using Interactable.Holdable;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,11 +11,15 @@ namespace entity
         [Header("WateringCan settings")]
         [SerializeField] private int wateringCanID;
         [SerializeField] private Transform rotationRoot;
+
+        [SerializeField] private Canvas interactionCanvas;
         
         [Header("Sound")]
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private AudioClip pickUpSound;
         [SerializeField] private AudioClip dropSound;
+
+        private Transform _playerCamera;
         
         private Quaternion _uprightRot;
         private Quaternion _pourRot;
@@ -23,6 +28,13 @@ namespace entity
 
         private readonly float _rotationAngle = 50f;
         private readonly float _rotationSpeed = 200f;
+
+        private void Awake()
+        {
+            if (interactionCanvas != null)
+                interactionCanvas.gameObject.SetActive(false);
+
+        }
 
         private void Start()
         {
@@ -37,14 +49,14 @@ namespace entity
 
             if (TryGetComponent<HoldableObjectBehaviour>(out var h))
                 isHeld = h.IsCurrentlyHeld;
-            
+
             var isMouseDown = isHeld && mouseClick;
 
             if (isHeld && !_hasTriggered)
             {
                 if (audioSource && pickUpSound)
                     audioSource.PlayOneShot(pickUpSound);
-                
+
                 ChoreEvents.TriggerWateringCanPickedUp(wateringCanID);
                 _hasTriggered = true;
             }
@@ -54,7 +66,31 @@ namespace entity
                 isMouseDown ? _pourRot : _uprightRot,
                 _rotationSpeed * Time.deltaTime
             );
+
+            if (interactionCanvas != null && interactionCanvas.gameObject.activeSelf && _playerCamera != null)
+            {
+                interactionCanvas.transform.LookAt(_playerCamera);
+                interactionCanvas.transform.Rotate(0f, 180f, 0f);
+            }
         }
+
+        /*public override void OnHoverEnter(IInteractor interactor)
+        {
+            base.OnHoverEnter(interactor);
+
+            bool canInteract = _holder;
+                
+            if (interactionCanvas != null)
+                interactionCanvas.gameObject.SetActive(canInteract);
+        }
+
+        public override void OnHoverExit(IInteractor interactor)
+        {
+            base.OnHoverExit(interactor);
+
+            if (interactionCanvas != null)
+                interactionCanvas.gameObject.SetActive(false);
+        }*/
     }
 }
 
