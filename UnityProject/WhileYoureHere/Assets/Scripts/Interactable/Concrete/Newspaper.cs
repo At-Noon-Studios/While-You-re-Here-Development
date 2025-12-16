37b28b04140e2043fe3c12f068489dcb6f430e5a
Stijn de Jong <s.dejong15@student.han.nl>
2025-12-16 12:48:45 +0100

    Newspaper

--- UnityProject/WhileYoureHere/Assets/Scripts/Interactable/Concrete/Newspaper.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/Interactable/Concrete/Newspaper.cs
@@ -5,21 +5,7 @@ using UnityEngine;
 
 namespace Interactable.Concrete
 {
-    [RequireComponent(typeof(AudioSource))]
     public class Newspaper : HoldableObjectBehaviour
     {
-        private AudioSource _audioSource;
-        
-        protected override void Awake()
-        {
-            base.Awake();
-            _audioSource = GetComponent<AudioSource>();
-        }
-        
-        public override void Interact(IInteractor interactor)
-        {
-            base.Interact(interactor);
-            _audioSource.Play();
-        }
     }
 }
