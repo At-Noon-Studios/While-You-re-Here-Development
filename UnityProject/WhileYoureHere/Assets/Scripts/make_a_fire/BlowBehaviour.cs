a2f796dc6b157c94e3f95bf12eb669c387359078
Davy de Bruin <davydb024@gmail.com>
2025-12-16 16:21:28 +0100

    Fixed Performance For BlowBehaviour.cs

--- UnityProject/WhileYoureHere/Assets/Scripts/make_a_fire/BlowBehaviour.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/make_a_fire/BlowBehaviour.cs
@@ -7,16 +7,17 @@ namespace make_a_fire
 {
     public class BlowBehaviour : MonoBehaviour
     {
-        [Header("Wind Configuration")] 
+        [Header("Wind Configuration")]
         [SerializeField] private VisualEffect windEffect;
         [SerializeField] private AudioClip blowAudio;
         [SerializeField] private EventChannel blowEvent;
         [SerializeField] private EventChannel blowAllowedEvent;
         [SerializeField] private StoveBehaviour furnace;
         [SerializeField] private float blowDistance = 2.5f;
-        
+
         private AudioSource _audioSource;
         private bool _canBlow;
+        private Coroutine _windRoutine;
 
         private void Awake()
         {
@@ -27,6 +28,10 @@ namespace make_a_fire
         private void OnEnable()
         {
             windEffect.Stop();
+            
+            blowEvent.OnRaise -= TryBlow;
+            blowAllowedEvent.OnRaise -= EnableBlow;
+
             blowEvent.OnRaise += TryBlow;
             blowAllowedEvent.OnRaise += EnableBlow;
         }
@@ -34,6 +39,13 @@ namespace make_a_fire
         private void OnDisable()
         {
             blowEvent.OnRaise -= TryBlow;
+            blowAllowedEvent.OnRaise -= EnableBlow;
+
+            if (_windRoutine != null)
+            {
+                StopCoroutine(_windRoutine);
+                _windRoutine = null;
+            }
         }
 
         private void EnableBlow()
@@ -41,21 +53,40 @@ namespace make_a_fire
             _canBlow = true;
         }
 
+        private void DisableBlow()
+        {
+            _canBlow = false;
+        }
+
         private void TryBlow()
         {
-            if (!_canBlow) return;
-            StartCoroutine(WindTimer());
+            if (!_canBlow)
+                return;
+            
+            if (_windRoutine != null)
+                StopCoroutine(_windRoutine);
+
+            _windRoutine = StartCoroutine(WindTimer());
 
             var blowDirection = transform.TransformDirection(Vector3.up);
             Debug.DrawLine(transform.position, transform.position + blowDirection * blowDistance, Color.cyan, 1f);
-            RaycastHit[] hits = Physics.RaycastAll(transform.position, blowDirection, blowDistance);
+
+            RaycastHit[] hits = Physics.RaycastAll(
+                transform.position,
+                blowDirection,
+                blowDistance
+            );
+
             foreach (var hit in hits)
             {
-                if (hit.collider.CompareTag("Player")) continue;
+                if (hit.collider.CompareTag("Player"))
+                    continue;
+
                 if (hit.collider.CompareTag("Furnace"))
                 {
                     furnace.StartBigFire();
-                    break; 
+                    DisableBlow();
+                    break;
                 }
             }
         }
@@ -64,9 +95,13 @@ namespace make_a_fire
         {
             windEffect.Play();
             _audioSource.PlayOneShot(blowAudio);
+
             yield return new WaitForSeconds(2f);
+
             windEffect.Stop();
             _audioSource.Stop();
+
+            _windRoutine = null;
         }
     }
 }
