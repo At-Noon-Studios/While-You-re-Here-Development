7c022d530c4a8026e4c095556ae1233675a205ca
Davy de Bruin <davydb024@gmail.com>
2026-01-07 17:15:37 +0100

    Fixed RadioCutscene

--- UnityProject/WhileYoureHere/Assets/Scripts/cutscenes/CutsceneController.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/cutscenes/CutsceneController.cs
@@ -4,16 +4,30 @@ using UnityEngine.Playables;
 public class CutsceneController : MonoBehaviour
 {
     [SerializeField] private PlayableDirector director;
-
+    
     void Awake()
     {
-        director.Stop();
-        director.time = 0;
-        director.Evaluate();
+        // Zorgt ervoor dat er bij scene load niets kan evalueren
+        director.playableAsset = null;
     }
 
-    public void PlayCutscene()
+    public void PlayCutscene(PlayableAsset timeline)
     {
+        if (timeline == null)
+        {
+            Debug.LogWarning("PlayCutscene called with null timeline", this);
+            return;
+        }
+
+        director.Stop();
+        director.playableAsset = timeline;
+        director.time = 0;
         director.Play();
+    }
+
+    public void StopCutscene()
+    {
+        director.Stop();
+        director.playableAsset = null;
     }
 }
