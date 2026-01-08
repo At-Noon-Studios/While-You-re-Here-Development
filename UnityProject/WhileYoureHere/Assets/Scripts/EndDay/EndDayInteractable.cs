4de9e385fe3f111da4d48807d43c51c4e9a54e6e
Stijn de Jong <s.dejong15@student.han.nl>
2026-01-08 12:03:57 +0100

    Day end sound

--- UnityProject/WhileYoureHere/Assets/Scripts/EndDay/EndDayInteractable.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/EndDay/EndDayInteractable.cs
@@ -13,6 +13,9 @@ namespace EndDay
         private CameraController _cameraController;
         private MovementController _movementController;
         private VideoPlayer _videoPlayer;
+        private AudioSource _audioSource;
+
+        [SerializeField] private AudioClip endOfDaySound;
 
         protected override void Awake()
         {
@@ -21,15 +24,17 @@ namespace EndDay
             _cameraController = player.GetComponentInChildren<CameraController>();
             _movementController = player.GetComponentInChildren<MovementController>();
             _videoPlayer = GetComponent<VideoPlayer>();
+            _audioSource = GetComponent<AudioSource>();
         }
-        
+
         public override void Interact(IInteractor interactor)
         {
             _cameraController.PauseCameraMovement();
             _movementController.PauseMovement();
             blockInteraction = true;
-            _videoPlayer.Play();
-            StartCoroutine(GoToNextScene());
+            // _videoPlayer.Play(); Not functional yet, commented to prevent error
+            _audioSource.PlayOneShot(endOfDaySound);
+            // StartCoroutine(GoToNextScene()); Not functional yet, commented to prevent error
         }
 
         private IEnumerator GoToNextScene()
@@ -38,7 +43,7 @@ namespace EndDay
             yield return new WaitForSeconds((float)_videoPlayer.clip.length);
             //go to new scene
         }
-        
+
         public override string InteractionText(IInteractor interactor)
         {
             return "End the day";
