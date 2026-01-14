cabb821d565c7dde70a909c4340e3d1c471a700b
nerhahon <nje.erhahon@student.han.nl>
2026-01-14 16:30:37 +0100

    Added animations onto the fishing rod :]

--- UnityProject/WhileYoureHere/Assets/Scripts/Fishing/FishingRod.cs
+++ UnityProject/WhileYoureHere/Assets/Scripts/Fishing/FishingRod.cs
@@ -18,6 +18,7 @@ namespace Fishing {
         private AudioSource _audioSource;
         private LineController _lineController;
         private Animator _animator;
+        private AnimatorStateInfo _state;
         private GameObject _spawnedFloater;
         private SoFish _caughtFish;
         private Action<Vector2> look;
@@ -35,28 +36,33 @@ namespace Fishing {
         private float _fishEscapeCount;
         private Vector3 _directionOfFloater;
         private readonly int[] _directions = {-1, 1};
+        
+        private float _reelAnimTimer;
+        private Quaternion[] _defaultRotations;
+        private Transform[] _bones;
 
         [Header("General settings")]
-        public float castingForce;
-        public float distanceFromShoreForCatch;
-        public float minReelTime;
-        public float floaterSpeed;
-        public float fishStruggleBeforeEscape;
-        public int reelWhileStrugglePunishment;
+        [SerializeField] private float castingForce;
+        [SerializeField] private float distanceFromShoreForCatch;
+        [SerializeField] private float minReelTime;
+        [SerializeField] private float floaterSpeed;
+        [SerializeField] private float fishStruggleBeforeEscape;
+        [SerializeField] private int reelWhileStrugglePunishment;
+        [SerializeField] private float _reelAnimHoldTime = 0.1f;
 
         [Header("Mouse settings")] 
-        public int minMouseToRightForCast;
-        public int minMouseUpForCast;
-        public int mouseMoveToCounterSteer;
+        [SerializeField] private int minMouseToRightForCast;
+        [SerializeField] private int minMouseUpForCast;
+        [SerializeField] private int mouseMoveToCounterSteer;
         
         [Header("Fishing rod objects")]
-        public GameObject line;
-        public GameObject fishingRodTop;
-        public GameObject floaterPrefab;
+        [SerializeField] private GameObject line;
+        [SerializeField] private GameObject fishingRodTop;
+        [SerializeField] private GameObject floaterPrefab;
         
         [Header("Caught fish voicelines")]
-        public List<AudioClip> success;
-        public List<AudioClip> fail;
+        [SerializeField] private List<AudioClip> success;
+        [SerializeField] private List<AudioClip> fail;
         
         public static event Action<SoFish> OnFishCaught;
         public static void TriggerFishCaught(SoFish fish) => OnFishCaught?.Invoke(fish);
@@ -66,7 +72,12 @@ namespace Fishing {
             base.Awake();
             var player = GameObject.FindWithTag("Player");
             if (player == null) return;
-            
+          
+            _bones = GetComponentsInChildren<Transform>();
+            _defaultRotations = new Quaternion[_bones.Length];
+            for (int i = 0; i < _bones.Length; i++) 
+                _defaultRotations[i] = _bones[i].localRotation;
+                
             _playerInput = player.GetComponent<PlayerInput>();
             _movementController = player.GetComponent<MovementController>();
             _cameraController = player.GetComponentInChildren<CameraController>();
@@ -81,6 +92,7 @@ namespace Fishing {
         private void FixedUpdate()
         {
             if (_holder == null) return;
+            
             if (_isCastPullPressed)
             {
                 if (!_isLineCast && !_isCasting) StartCast();
@@ -101,6 +113,30 @@ namespace Fishing {
                  if (_fishEscapeCount >= fishStruggleBeforeEscape) FishEscape();
              }
              else _fishEscapeCount = Mathf.Max(0, _fishEscapeCount - 1);
+        }
+        
+        private void Update()
+        {
+            if (_animator == null) return;
+            
+            if (_holder == null)
+            {
+                _animator.enabled = false;
+                return;
+            }
+            
+            if (!_animator.enabled) 
+            { 
+                _animator.enabled = true;
+                _animator.Play("No Rod", 0, 0f); 
+            }
+
+            _state = _animator.GetCurrentAnimatorStateInfo(0);
+            if (_reelAnimTimer > 0f)
+                _reelAnimTimer -= Time.deltaTime;
+
+            bool isReelingAnim = _reelAnimTimer > 0f;
+            _animator.SetBool("ReelInFish", isReelingAnim);
         }
 
         private void StartCast()
@@ -144,11 +180,13 @@ namespace Fishing {
 
         private void CastLine()
         {
+            Debug.Log("Now casting line !");
+            _animator.SetTrigger("Castbitch");
             _isCasting = false;
             _isLineCast = true;
             _spawnedFloater = Instantiate(floaterPrefab, fishingRodTop.transform.position, fishingRodTop.transform.rotation);
             _spawnedFloater.gameObject.GetComponent<Rigidbody>().AddForce(castingForce * _playerCamera.forward, ForceMode.Impulse);
-            _lineController.SetUpLine(new []{fishingRodTop.transform, _spawnedFloater.transform});
+            // _lineController.SetUpLine(new []{fishingRodTop.transform, _spawnedFloater.transform});
             OnFishCaught += ListenForFishCaught;
             line.SetActive(false);
             ResetPose();
@@ -160,6 +198,7 @@ namespace Fishing {
             camPosition.y = _spawnedFloater.transform.position.y;
             if (Physics.Raycast(_spawnedFloater.transform.position, camPosition - _spawnedFloater.transform.position, out var hit))
             {
+                _animator.SetTrigger("FishBite");
                 _reelTargetPosition = hit.point;
                 _caughtFish = fish;
                 _currentReelSpeed = Vector3.Distance(_reelTargetPosition, _spawnedFloater.transform.position) / minReelTime;
@@ -195,12 +234,18 @@ namespace Fishing {
             FloaterController.TriggerFloaterMove(_currentFloaterMovement);
             if (Vector3.Distance(_reelTargetPosition, _spawnedFloater.transform.position) <= distanceFromShoreForCatch)
             {
-                _animator.Play("Catch");
-                Instantiate(_caughtFish.fishPrefab, fishingRodTop.transform.position, fishingRodTop.transform.rotation);
-                _audioSource.PlayOneShot(success[Random.Range(0, success.Count)]);
-                _caughtFish = null;
-                Debug.Log(_animator.GetCurrentAnimatorClipInfo(0));
-                StartCoroutine(ReturnLineAfterDelay(_animator.GetCurrentAnimatorClipInfo(0).Length));
+                _animator.SetTrigger("CatchFish");
+                if (_state.IsName("Catch") && _state.normalizedTime >= 0.49)
+                {
+                    Instantiate(_caughtFish.fishPrefab, fishingRodTop.transform.position, fishingRodTop.transform.rotation);
+                    _audioSource.PlayOneShot(success[Random.Range(0, success.Count)]);
+                    _caughtFish = null;
+                    Debug.Log(_animator.GetCurrentAnimatorClipInfo(0));
+                    StartCoroutine(ReturnLineAfterDelay(_animator.GetCurrentAnimatorClipInfo(0).Length));
+                    _animator.ResetTrigger("CatchFish");
+                }
+                // if (_state.IsName("Catch") && _state.normalizedTime >= 1f) _animator.ResetTrigger("CatchFish");
+                // Debug.Log(_state.normalizedTime);
             }
         }
 
@@ -223,12 +268,18 @@ namespace Fishing {
             look -= UpdateMouseForCounterSteer;
             _cameraController.ResumeCameraMovement();
             _movementController.ResumeMovement();
+            _animator.Play("No Rod");
+            _animator.ResetTrigger("CatchFish");
+            // ResetBones();
         }
 
         private void OnReelFishingRod()
         {
             if (!_isLineCast) return;
             OnFishCaught -= ListenForFishCaught;
+            
+            _reelAnimTimer = _reelAnimHoldTime;
+
             if (_caughtFish != null) 
             {
                 ReelInFish();
@@ -267,12 +318,20 @@ namespace Fishing {
             OnFishCaught -= ListenForFishCaught;
             _audioSource.PlayOneShot(fail[Random.Range(0, fail.Count)]);
             ReturnLine();
+            _animator.Play("No Rod");
+            _animator.ResetTrigger("CatchFish");
         }
 
         public override void Drop()
         {
             if (_isLineCast) return;
             base.Drop();
+        }
+
+        private void ResetBones()
+        {
+            for (int i = 0; i < _bones.Length; i++)
+                _bones[i].localRotation = _defaultRotations[i];
         }
     }
 }
