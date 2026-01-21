9da3032e5c3ce5b6ee51828d7a04632549d79193
WouterM4 <117893069+WouterM4@users.noreply.github.com>
2026-01-21 14:21:54 +0100

    refactor refactorrrr

---
+++ UnityProject/WhileYoureHere/Assets/Scripts/Interactable/Concrete/ObjectHolder/IObjectHolder.cs
@@ -0,0 +1,10 @@
+﻿using Interactable.Holdable;
+using UnityEngine;
+
+namespace Interactable.Concrete.ObjectHolder
+{
+    public interface IObjectHolder
+    {
+        public void ClearHeldObject(GameObject obj);
+    }
+}
